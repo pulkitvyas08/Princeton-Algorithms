1fa5a1a851a1c1152a22e0806b528eb08fd62379
pulkitvyas08 <e19cse284@bennett.edu.in>
2020-04-25 16:27:23 +0530

    first (submitted) draft of percolation

--- algorithms-part-i/week-1/percolation/Percolation.java
+++ algorithms-part-i/week-1/percolation/Percolation.java
@@ -1,43 +1,80 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
     private int n;
+    private final int topRow;
+    private final int bottomRow;
+    private WeightedQuickUnionUF grid;
+    private WeightedQuickUnionUF gridCheck;
+    private boolean[][] cells;
+    private int numOpenSites;
 
     public Percolation(int n) {
-        this.n = n;
         if (n <= 0)
             throw new IllegalArgumentException();
+        this.n = n;
+        numOpenSites = 0;
+        grid = new WeightedQuickUnionUF(n * n + 2);
+        gridCheck = new WeightedQuickUnionUF(n * n + 2);
+        topRow = 0;
+        bottomRow = n * n;
+        cells = new boolean[n][n];
     }
 
-    public void open(int row, int col) throws IllegalArgumentException {
+    public void open(int row, int col) {
         if (row <= 0 || row >= n || col <= 0 || col >= n)
             throw new IllegalArgumentException();
+        if (!isOpen(row, col)) {
+            numOpenSites++;
+            cells[row - 1][col - 1] = true;
+
+            if (row == 1) {
+                grid.union(topRow, dimReducer(row, col));
+                gridCheck.union(topRow, dimReducer(row, col));
+            }
+            if (row == n)
+                grid.union(bottomRow, dimReducer(row, col));
+            if (row > 1 && isOpen(row - 1, col)) {
+                grid.union(dimReducer(row, col), dimReducer(row - 1, col));
+                gridCheck.union(dimReducer(row, col), dimReducer(row - 1, col));
+            }
+            if (row < n && isOpen(row + 1, col)) {
+                grid.union(dimReducer(row, col), dimReducer(row + 1, col));
+                gridCheck.union(dimReducer(row, col), dimReducer(row + 1, col));
+            }
+            if (col > 1 && isOpen(row, col - 1)) {
+                grid.union(dimReducer(row, col), dimReducer(row, col - 1));
+                gridCheck.union(dimReducer(row, col), dimReducer(row, col - 1));
+            }
+            if (col < n && isOpen(row, col + 1)) {
+                grid.union(dimReducer(row, col), dimReducer(row, col + 1));
+                gridCheck.union(dimReducer(row, col), dimReducer(row, col + 1));
+            }
+        }
     }
 
-    public boolean isOpen(int row, int col) throws IllegalArgumentException {
+    public boolean isOpen(int row, int col) {
         if (row <= 0 || row >= n || col <= 0 || col >= n)
             throw new IllegalArgumentException();
+        return cells[row - 1][col - 1];
     }
 
     public boolean isFull(int row, int col) {
         if (row <= 0 || row >= n || col <= 0 || col >= n)
             throw new IllegalArgumentException();
+        return (isOpen(row, col) && gridCheck.connected(topRow, dimReducer(row, col)));
     }
 
     public int numberOfOpenSites() {
-
+        return numOpenSites;
     }
 
     public boolean percolates() {
-
+        return grid.connected(topRow, bottomRow);
     }
 
-    public static void main(String[] args) {
-
+    private int dimReducer(int row, int col) {
+        return (row - 1) * n + col;
     }
 }
--- algorithms-part-i/week-1/percolation/PercolationStats.java
+++ algorithms-part-i/week-1/percolation/PercolationStats.java
@@ -1,33 +1,55 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-    public PercolationStats(int n, int trials) throws IllegalArgumentException {
+    private static final double CONFIDENCEINTV95 = 1.96;
+    private final int trials;
+    private final double[] gridValue;
+
+    public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0)
             throw new IllegalArgumentException();
+        this.trials = trials;
+        gridValue = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(n);
+            while (!perc.percolates()) {
+                int cellOne = StdRandom.uniform(1, n + 1);
+                int cellTwo = StdRandom.uniform(1, n + 1);
+                if (!perc.isOpen(cellOne, cellTwo))
+                    perc.open(cellOne, cellTwo);
+            }
+            gridValue[i] = (double) perc.numberOfOpenSites() / (n * n);
+        }
     }
 
     public double mean() {
-
+        return StdStats.mean(gridValue);
     }
 
     public double stddev() {
-
+        return StdStats.stddev(gridValue);
     }
 
     public double confidenceLo() {
-
+        return mean() - (CONFIDENCEINTV95 * stddev() / Math.sqrt(trials));
     }
 
     public double confidenceHi() {
-
+        return mean() + (CONFIDENCEINTV95 * stddev() / Math.sqrt(trials));
     }
 
     public static void main(String[] args) {
 
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        PercolationStats percStats = new PercolationStats(n, trials);
+        System.out.printf("%-23s = %f%n", "mean", percStats.mean());
+        System.out.printf("%-23s = %f%n", "stddev", percStats.stddev());
+        System.out.printf("%-23s = [%f, %f]", "95% confidence interval", percStats.confidenceLo(),
+                          percStats.confidenceHi());
     }
 }
